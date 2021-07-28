6b438bf2b10ecf64a472a7568f420df2ca6eab1c
Patel Akash <patelakash56964@gmail.com>
2021-07-28 17:12:53 +0530

    update

--- custom_addons/school/models/school.py
+++ custom_addons/school/models/school.py
@@ -2,7 +2,8 @@ from odoo import models,fields
 
 class SchoolProfile(models.Model):
     _name = 'school.profile'
+    _description = 'School Information'
 
-    name = fields.Char(string='School name')
+    school_name = fields.Char(string='School name',help='Write a school name',default = 'Must write',size = 15,index=True,)
     email = fields.Char(string='Email')
-    phonegf = fields.Char()
+    phone = fields.Char(string='Phone')
