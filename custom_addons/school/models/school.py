0b4fcab99fe79ef462778db4d4b814673954d149
Patel Akash <patelakash56964@gmail.com>
2021-07-28 18:18:05 +0530

    update

--- custom_addons/school/models/school.py
+++ custom_addons/school/models/school.py
@@ -4,6 +4,6 @@ class SchoolProfile(models.Model):
     _name = 'school.profile'
     _description = 'School Information'
 
-    school_name = fields.Char(string='School name',help='Write a school name',default = 'Must write',size = 15,index=True,)
+    school_name = fields.Char(string='School name',help='Write a school name',required = True,trim = False,index=True,)
     email = fields.Char(string='Email')
     phone = fields.Char(string='Phone')
