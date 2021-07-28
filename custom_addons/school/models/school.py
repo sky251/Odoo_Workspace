b48b55d3e280daccb0317251508e843864c8de67
Patel Akash <patelakash56964@gmail.com>
2021-07-28 14:55:05 +0530

    Update school.py

--- custom_addons/school/models/school.py
+++ custom_addons/school/models/school.py
@@ -3,6 +3,6 @@ from odoo import models,fields
 class SchoolProfile(models.Model):
     _name = 'school.profile'
 
-    name = fields.Char(string='school name')
+    name = fields.Char(string='School name')
     email = fields.Char(string='Email')
-    phone = fields.Char('Phone')
+    phonegf = fields.Char()
