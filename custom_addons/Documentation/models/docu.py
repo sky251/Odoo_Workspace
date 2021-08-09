b8742e69b0bbe9220150c7a511f4fa382716a76d
Patel Akash <patelakash56964@gmail.com>
2021-08-09 10:10:47 +0530

    update

--- custom_addons/Documentation/models/docu.py
+++ custom_addons/Documentation/models/docu.py
@@ -1,12 +1,19 @@
-from odoo import models, fields
+from odoo import api, fields, models
+
 
 class Documentation(models.Model):
     _name = "docu.item"
     _description = "This is documentation task"
 
-    name = fields.Char(string = "Document name")
-
-    description_sort = fields.Char(string = "Sort description")
-    description_long = fields.Char(string = "Long description")
+    name = fields.Char(string="Document name")
+    description_sort = fields.Text(string="Sort description")
+    description_long = fields.Html(string="Long description")
     video_url = fields.Char(string="Video url")
     google_document_url = fields.Char(string="Google doc url")
+
+    @api.model
+    def _selection_languages(self):
+        return self.env['res.lang'].get_installed()
+
+
+    lang = fields.Selection(_selection_languages, string='Template Preview Language')
