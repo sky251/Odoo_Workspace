df44a4d42a59006a177329c39c9f235996fd3e74
Patel Akash <patelakash56964@gmail.com>
2021-08-12 16:54:59 +0530

    update

--- custom_addons/school/models/student.py
+++ custom_addons/school/models/student.py
@@ -21,8 +21,9 @@ class StudentProfile(models.Model):
     user_signature = fields.Binary(string='Signature')
     is_parking = fields.Boolean(related="school_select_id.parking", string="Is parking", store=True)
     calculate = fields.Integer(compute="_compute_total_calcu", string="Calculate")
-    state = fields.Selection([('create', 'Create'), ('received', 'Received'), ('done', 'Done'), ('cancelled', 'Cancelled')],
-                             default='create', string="Status")
+    state = fields.Selection(
+        [('create', 'Create'), ('received', 'Received'), ('done', 'Done'), ('cancelled', 'Cancelled')],
+        default='create', string="Status")
 
     def action_confirm(self):
         self.state = 'received'
@@ -32,3 +33,15 @@ class StudentProfile(models.Model):
 
     def action_cancel(self):
         self.state = 'cancelled'
+
+    def clear_record_data(self):
+        self.write({
+            'name': '',
+            'student_email': '',
+            'student_phone': '',
+            'student_result': '',
+            'student_img': '',
+            'user_signature': '',
+            'is_parking': '',
+            'calculate': '',
+        })
