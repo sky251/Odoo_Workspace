eac51964a69f10304161461767867431e4d872de
Patel Akash <patelakash56964@gmail.com>
2021-09-29 18:39:33 +0530

    update

--- custom_addons/product_website/controllers/product_controllers.py
+++ custom_addons/product_website/controllers/product_controllers.py
@@ -4,7 +4,7 @@ from odoo.http import request, route
 
 
 class Controller(http.Controller):
-    @http.route("/product", type="http", website=True, auth="public")
+    @http.route("/product/", type="http", website=True, auth="public")
     def product_details(self):
         print("\n\n\n\n\n\n Hello this is Product page\n\n\n\n\n\n")
         product_data = request.env["product.template"].search([])
