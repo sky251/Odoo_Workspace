a62f8bba43383fb08c1964c8db2d1e56a51338c2
Patel Akash <patelakash56964@gmail.com>
2021-10-07 18:36:55 +0530

    UPDATE

--- custom_addons/a_res_partner/models/order.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, fields, models
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    sssss = fields.Boolean(string="Bool")
--- custom_addons/product_website/models/res_partner.py
+++ custom_addons/product_website/models/res_partner.py
@@ -6,7 +6,10 @@ class TeamMember(models.Model):
     _description = "Team member"
 
     partner_id = fields.Many2one("res.partner", string="Partner")
-
+    name = fields.Char(related="member_id.name", string="Name")
+    email = fields.Char(related="member_id.email", string="Name")
+    phone = fields.Char(related="member_id.phone", string="Name")
+    member_id = fields.Many2one("res.partner", string="Member")
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
@@ -15,5 +18,4 @@ class ResPartner(models.Model):
         "team.member", "partner_id", string="Team member id"
     )
 
-    boolean_tl = fields.Boolean(string="TL", default=True)
-
+    boolean_tl = fields.Boolean(string="TL")
